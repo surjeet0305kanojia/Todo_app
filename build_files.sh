17f838fd5d82c8083fde56948b05ee51342523ba
Surjeet kumar kanojia <surjeet0305kanojia@gmail.com>
2023-07-31 00:17:03 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 # build_files.sh
 echo " BUILD START"
-
-python3.9 -m pip install -r requirements.txt
+pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
