da4ea40204978c35f0f3e43eb8fff8b7002263a4
Surjeet kumar kanojia <surjeet0305kanojia@gmail.com>
2023-08-15 17:49:07 +0530

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': os.getenv('postgresql://postgres:QvzYPsU1dBM9MuGX7scs@containers-us-west-118.railway.app:7067/railway'),
+        'NAME': os.getenv('railway'),
+        'USER': os.getenv('postgres'),
+        'PASSWORD': os.getenv('QvzYPsU1dBM9MuGX7scs'),
+        'HOST': os.getenv('containers-us-west-118.railway.app'),
+        'PORT': os.getenv('7067'),
     }
 }
 
